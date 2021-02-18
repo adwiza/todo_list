04c5ddf8b0caaf73f3b370183d97e2e46cd4b259
Acid Wizard <adwiz@yandex.ru>
2021-02-18 11:15:31 +0300

    completed and delete now working

--- server.py
+++ server.py
@@ -1,13 +1,22 @@
-from bottle import route, run, view, static_file
+from bottle import route, run, view, static_file, redirect
 
 
 class TodoItem:
-    def __init__(self, description):
+    def __init__(self, description, unique_id):
         self.description = description
         self.is_completed = False
+        self.uid = unique_id
 
     def __str__(self):
-        return self.description.lower()
+        return self.description   # .lower()
+
+
+tasks_db = {
+        1: TodoItem('Прочитать книгу', 1),
+        2: TodoItem('Учиться жонглировать 30 минут', 2),
+        3: TodoItem('Помыть посуду', 3),
+        4: TodoItem('Поесть', 4),
+    }
 
 
 @route('/static/<filename:path>')
@@ -18,13 +27,21 @@ def send_static(filename):
 @route('/')
 @view('index')
 def index():
-    tasks = [
-        TodoItem('Прочитать книгу'),
-        TodoItem('Учиться жонглировать 30 минут'),
-        TodoItem('Помыть посуду'),
-        TodoItem('Поесть'),
-    ]
+    tasks = tasks_db.values()
     return {'tasks': tasks}
+
+
+@route('/api/delete/<uid:int>')
+def api_delete(uid):
+    tasks_db.pop(uid)
+    return redirect('/')
+
+
+@route('/api/complete/<uid:int>')
+def api_complete(uid):
+    tasks_db[uid].is_completed = True
+    return 'OK'
+
 
 ###
 run(host='localhost', port=8080)
--- static/script.js
+++ static/script.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
-$(document).on('click', '.remove', function(){
-$(this).parent().remove();
-})
-})
 
-$(document).on('click', '.checkbox', function() {
-$(this).parent().addClass('completed');
-$(this).attr('disabled', true);
+        $(document).on('click', '.checkbox', function() {
+                $(this).parent().addClass('completed');
+                $(this).attr('disabled', true);
+                uid = $(this).attr('data-uid');
+                $.get("/api/complete/" + uid);
+        });
+        $(document).on('click', '.remove', function(){
+                $(this).parent().remove();
+        });
+
 });
